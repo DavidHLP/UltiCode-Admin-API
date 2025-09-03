3bba504c36a902f23881026b55e01a858dacd00e
DavidHLP <lysf15520112973@163.com>
2025-09-03 21:21:11 +0800

    feat(common): 添加负载均衡配置并优化 Feign 客户端- 在 application-cloud.yml 文件中添加负载均衡器缓存禁用配置 - 修改 sandbox 服务名称- 在 FeignConfig 中添加重试器配置 - 新增 LoadBalancerConfiguration 类配置随机负载均衡器 - 更新 pom.xml 文件，替换 OpenFeign 依赖为 LoadBalancer - 在 ProblemsService 中添加负载均衡客户端注解

--- SpringOJ-problems/src/main/java/com/david/ProblemsServiceSpringBootApplication.java
+++ SpringOJ-problems/src/main/java/com/david/ProblemsServiceSpringBootApplication.java
@@ -1,11 +1,15 @@
 package com.david;
 
+import com.david.config.LoadBalancerConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@LoadBalancerClient(name = "user-service", configuration = LoadBalancerConfiguration.class)
 public class ProblemsServiceSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProblemsServiceSpringBootApplication.class, args);
